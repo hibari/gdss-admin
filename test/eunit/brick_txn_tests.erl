d0cb78851decadb542622b3ae272d5a8c53e767c
Joseph Wayne Norton <norton@alum.mit.edu>
2011-04-13 12:22:56 +0900

    Update simple txn test

--- test/eunit/brick_txn_tests.erl
+++ test/eunit/brick_txn_tests.erl
@@ -28,6 +28,10 @@ all_tests_test_() ->
      fun test_teardown/1,
      [
       ?_test(test_simple_txn1())
+      , ?_test(test_simple_txn2a())
+      , ?_test(test_simple_txn2b())
+      , ?_test(test_simple_txn3a())
+      , ?_test(test_simple_txn3b())
      ]}.
 
 test_setup() ->
@@ -37,6 +41,8 @@ test_teardown(X) ->
     brick_eunit_utils:teardown(X),
     ok.
 
+%% @doc set transaction with TS less than old timestamp ... this
+%% should fail
 test_simple_txn1() ->
     KeyPrefix = <<"/100/1">>,
 
@@ -68,6 +74,158 @@ test_simple_txn1() ->
 
     %% get_many
     {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}
+           , {KeyB, TS3, ValA, 0, [{val_len,ValALen}]} %% TODO: bug!
+          ], false}} = brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    ok.
+
+%% @doc set transaction with TS equal to old timestamp but different
+%% value ... this should fail
+test_simple_txn2a() ->
+    KeyPrefix = <<"/100/1">>,
+
+    KeyA = <<"/100/1/A">>,
+    ValA = <<"AAA">>,
+    ValALen = byte_size(ValA),
+
+    KeyB = <<"/100/1/B">>,
+    ValB = <<"BBB">>,
+    _ValBLen = byte_size(ValB),
+
+    %% reset
+    _ = brick_simple:delete(tab1, KeyA),
+    _ = brick_simple:delete(tab1, KeyB),
+
+    %% add KeyA
+    ok = brick_simple:add(tab1, KeyA, ValA),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}], false}} =
+        brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    %% txn set KeyA and set KeyB
+    TS2 = TS1,
+    SetA = brick_server:make_set(KeyA, TS2, ValB, 0, []),
+    TS3 = 2,
+    SetB = brick_server:make_set(KeyB, TS3, ValA, 0, []),
+    [{ts_error,TS1}, ok] = brick_simple:do(tab1, [txn, SetA, SetB]),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}
+           , {KeyB, TS3, ValA, 0, [{val_len,ValALen}]} %% TODO: bug!
+          ], false}} = brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    ok.
+
+%% @doc set transaction with TS equal to old timestamp but same
+%% value ... this should succeed
+test_simple_txn2b() ->
+    KeyPrefix = <<"/100/1">>,
+
+    KeyA = <<"/100/1/A">>,
+    ValA = <<"AAA">>,
+    ValALen = byte_size(ValA),
+
+    KeyB = <<"/100/1/B">>,
+    ValB = <<"BBB">>,
+    _ValBLen = byte_size(ValB),
+
+    %% reset
+    _ = brick_simple:delete(tab1, KeyA),
+    _ = brick_simple:delete(tab1, KeyB),
+
+    %% add KeyA
+    ok = brick_simple:add(tab1, KeyA, ValA),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}], false}} =
+        brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    %% txn set KeyA and set KeyB
+    TS2 = TS1,
+    SetA = brick_server:make_set(KeyA, TS2, ValA, 0, []),
+    TS3 = 2,
+    SetB = brick_server:make_set(KeyB, TS3, ValA, 0, []),
+    [ok, ok] = brick_simple:do(tab1, [txn, SetA, SetB]),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}
+           , {KeyB, TS3, ValA, 0, [{val_len,ValALen}]}
+          ], false}} = brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    ok.
+
+%% @doc set transaction with TS larger than old timestamp but different
+%% value ... this should succeed
+test_simple_txn3a() ->
+    KeyPrefix = <<"/100/1">>,
+
+    KeyA = <<"/100/1/A">>,
+    ValA = <<"AAA">>,
+    ValALen = byte_size(ValA),
+
+    KeyB = <<"/100/1/B">>,
+    ValB = <<"BBB">>,
+    ValBLen = byte_size(ValB),
+
+    %% reset
+    _ = brick_simple:delete(tab1, KeyA),
+    _ = brick_simple:delete(tab1, KeyB),
+
+    %% add KeyA
+    ok = brick_simple:add(tab1, KeyA, ValA),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}], false}} =
+        brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    %% txn set KeyA and set KeyB
+    TS2 = TS1+1,
+    SetA = brick_server:make_set(KeyA, TS2, ValB, 0, []),
+    TS3 = 2,
+    SetB = brick_server:make_set(KeyB, TS3, ValA, 0, []),
+    [ok, ok] = brick_simple:do(tab1, [txn, SetA, SetB]),
+
+    %% get_many
+    {ok, {[{KeyA, TS2, ValB, 0, [{val_len,ValBLen}]}
+           , {KeyB, TS3, ValA, 0, [{val_len,ValALen}]}
+          ], false}} = brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    ok.
+
+%% @doc set transaction with TS larger than old timestamp but same
+%% value ... this should succeed
+test_simple_txn3b() ->
+    KeyPrefix = <<"/100/1">>,
+
+    KeyA = <<"/100/1/A">>,
+    ValA = <<"AAA">>,
+    ValALen = byte_size(ValA),
+
+    KeyB = <<"/100/1/B">>,
+    ValB = <<"BBB">>,
+    _ValBLen = byte_size(ValB),
+
+    %% reset
+    _ = brick_simple:delete(tab1, KeyA),
+    _ = brick_simple:delete(tab1, KeyB),
+
+    %% add KeyA
+    ok = brick_simple:add(tab1, KeyA, ValA),
+
+    %% get_many
+    {ok, {[{KeyA, TS1, ValA, 0, [{val_len,ValALen}]}], false}} =
+        brick_simple:get_many(tab1, KeyPrefix, 100),
+
+    %% txn set KeyA and set KeyB
+    TS2 = TS1+1,
+    SetA = brick_server:make_set(KeyA, TS2, ValA, 0, []),
+    TS3 = 2,
+    SetB = brick_server:make_set(KeyB, TS3, ValA, 0, []),
+    [ok, ok] = brick_simple:do(tab1, [txn, SetA, SetB]),
+
+    %% get_many
+    {ok, {[{KeyA, TS2, ValA, 0, [{val_len,ValALen}]}
            , {KeyB, TS3, ValA, 0, [{val_len,ValALen}]}
           ], false}} = brick_simple:get_many(tab1, KeyPrefix, 100),
 
